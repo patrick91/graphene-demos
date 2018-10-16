1e65390dfe2164a1879d9828b5cb5e0546e8c0a0
Patrick Arminio <patrick.arminio@gmail.com>
2018-10-16 12:57:30 -0700

    Add create poll mutation

--- polls/mutations.py
+++ polls/mutations.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django.forms.mutation import DjangoFormMutation
 
-from .models import Poll
+from .models import Poll, Choice
 from .forms import VoteForm
 from .types import PollType
 
@@ -17,5 +17,22 @@ class Vote(DjangoFormMutation):
         return Poll.objects.first()
 
 
+class CreatePoll(graphene.Mutation):
+    poll = graphene.Field(PollType)
+
+    class Arguments:
+        question = graphene.String(required=True)
+        choices = graphene.List(graphene.String, required=True)
+
+    def mutate(self, info, question, choices):
+        poll = Poll.objects.create(question=question)
+
+        for choice in choices:
+            Choice.objects.create(poll=poll, choice_text=choice)
+
+        return CreatePoll(poll)
+
+
 class PollsMutations:
     vote = Vote.Field()
+    create_poll = CreatePoll.Field()
--- polls/schema.py
+++ polls/schema.py
@@ -4,8 +4,12 @@ from .models import Poll
 from .types import PollType
 
 
-class PollsQuery(object):
+class PollsQuery:
     polls = graphene.List(graphene.NonNull(PollType))
+    poll = graphene.Field(PollType, id=graphene.ID())
 
     def resolve_polls(self, info):
         return Poll.objects.all()
+
+    def resolve_poll(self, info, id):
+        return Poll.objects.get(pk=id)
